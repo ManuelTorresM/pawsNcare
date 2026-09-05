610f0e37560078900914f8658336f0e8926ccead
Manuel Torres <148628531+ManuelTorresM@users.noreply.github.com>
2026-09-05 18:02:38 +0200

    fix(android): enforce Java 17 compileOptions across subprojects to eliminate obsolete Java 8 warnings

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,19 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.library")) {
+            configure<com.android.build.gradle.LibraryExtension> {
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
